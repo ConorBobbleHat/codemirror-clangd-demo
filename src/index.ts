f4a6e18bed7c882bab7f8d4d5665c046f28643b7
ConorBobbleHat <c.github@firstpartners.net>
2023-03-24 19:52:29 +0000

    Enable tab indentation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { EditorView, basicSetup } from "codemirror"
-import { cpp } from "@codemirror/lang-cpp"
-import { languageServerWithTransport } from 'codemirror-languageserver'
-import PostMessageWorkerTransport from "./postMessageWorkerTransport";
+import { basicSetup } from "codemirror"
+import { EditorView, keymap } from "@codemirror/view";
+import { indentWithTab } from "@codemirror/commands";
 import { lintGutter } from "@codemirror/lint";
 import { autocompletion } from "@codemirror/autocomplete";
+import { cpp } from "@codemirror/lang-cpp"
+
+import { languageServerWithTransport } from 'codemirror-languageserver'
+import PostMessageWorkerTransport from "./postMessageWorkerTransport";
 
 const ls = languageServerWithTransport({
 	transport: new PostMessageWorkerTransport(new Worker(new URL('./lsp.worker.ts', import.meta.url))) as any,
@@ -19,7 +22,7 @@ const fullscreenEditor = EditorView.theme({
 })
 
 let editor = new EditorView({
-	extensions: [basicSetup, fullscreenEditor, cpp(), lintGutter(), ls, autocompletion()],
+	extensions: [basicSetup, fullscreenEditor, cpp(), lintGutter(), ls, autocompletion(), keymap.of([indentWithTab])],
 	parent: document.body,
 	doc: 'int main()\n{\n  return "something that\'s *definitely* not an int";\n}',
 })
